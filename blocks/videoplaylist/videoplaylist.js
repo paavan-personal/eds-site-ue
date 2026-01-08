003fc1b7810c46567a2e94738dfd5c371296b038
IN010149470_EYGS <21paavan@gmail.com>
2026-01-08 12:32:55 +0530

    indentation issue

--- blocks/videoplaylist/videoplaylist.js
+++ blocks/videoplaylist/videoplaylist.js
@@ -1,11 +1,10 @@
 export default async function decorate(block) {
-    const rows = [...block.children];
-    rows.forEach((row, index) => {
-        const cells = [...row.children];
-        if (cells.length === 0) return;
-
-        const cell = cells[index];
-        const cellContent = cell.textContent.trim();
-        console.log(cellContent);
-    });
+  const rows = [...block.children];
+  rows.forEach((row, index) => {
+    const cells = [...row.children];
+    if (cells.length === 0) return;
+    const cell = cells[index];
+    const cellContent = cell.textContent.trim();
+    console.log(cellContent);
+  });
 }
