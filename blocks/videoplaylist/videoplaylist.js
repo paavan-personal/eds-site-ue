60283e4f764e7369e5956644a1d538d4c43bd66e
IN010149470_EYGS <21paavan@gmail.com>
2026-01-08 16:00:52 +0530

    Update videoplaylist.js

--- blocks/videoplaylist/videoplaylist.js
+++ blocks/videoplaylist/videoplaylist.js
@@ -1,8 +1,13 @@
 export default async function decorate(block) {
  const rows = [...block.children];
- for(const row of rows){
-   const link = row.querySelector("a");
-   const image = row.querySelector("picture");
-   console.log(link, image);
- }
+ rows.forEach(row => {
+    const cells = [...row.children];
+    if (cells.length === 0) return;
+
+    const cell = cells[0];
+    const cellContent = cell.textContent.trim();
+    const linkElement = cell.querySelector('a');
+    const pictureElement = cell.querySelector('picture');
+    console.log(cellContent,linkElement,pictureElement);
+ });
 }
