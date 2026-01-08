cbd2c86b5024d3b960f0703944c1dd4954677f01
IN010149470_EYGS <21paavan@gmail.com>
2026-01-08 12:30:36 +0530

    lint issue fix

--- blocks/videoplaylist/videoplaylist.js
+++ blocks/videoplaylist/videoplaylist.js
@@ -1,14 +1,11 @@
 export default async function decorate(block) {
-  const rows = [...block.children];
-  let link = '';
-  let placeholder = null;
+    const rows = [...block.children];
+    rows.forEach((row, index) => {
+        const cells = [...row.children];
+        if (cells.length === 0) return;
 
-rows.forEach((row, index) => {
-    const cells = [...row.children];
-    if (cells.length === 0) return;
-
-    const cell = cells[0];
-    const cellContent = cell.textContent.trim();
-    console.log(cellContent);
-  });
+        const cell = cells[index];
+        const cellContent = cell.textContent.trim();
+        console.log(cellContent);
+    });
 }
