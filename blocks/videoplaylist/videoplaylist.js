34ef223679a52b791f1c321f390a84b792754c5b
IN010149470_EYGS <21paavan@gmail.com>
2026-01-08 14:33:12 +0530

    testing

--- blocks/videoplaylist/videoplaylist.js
+++ blocks/videoplaylist/videoplaylist.js
@@ -1,4 +1,6 @@
 export default async function decorate(block) {
+  const link = block.children[0].querySelector("a");
+  const image = block.children[0].querySelector("picture");
   const rows = [...block.children];
   rows.forEach((row, index) => {
     const cells = [...row.children];
